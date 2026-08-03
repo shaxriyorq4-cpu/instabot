703dac0667d26a56a071f2ff774dc4ac4457ee00
shaxriyorq4-cpu <shaxriyorq4@gmail.com>
2026-08-03 17:43:23 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,21 @@
-import asyncio
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import Command
+import asyncio
 
 TOKEN = "8915219066:AAGSCkzvFImev5HLBdOMqv-q8CWjraGnsHg"
 
-bot = Bot(token=TOKEN)
+bot = Bot(TOKEN)
 dp = Dispatcher()
 
-@dp.message(Command("start"))
-async def start_handler(message: types.Message):
-    await message.answer("Salom")
 
-@dp.message()
-async def echo_handler(message: types.Message):
-    await message.answer("Salom")
+@dp.message(Command("start"))
+async def start(message: types.Message):
+    await message.answer("Salom 👋")
+
 
 async def main():
-    print("🚀 Bot ishga tushdi...")
     await dp.start_polling(bot)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
